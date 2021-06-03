c6df3bcbba3038b00ce12d119117a198c471659b
jadesa <jadeesaa@gmail.com>
2021-06-03 20:46:44 +0300

    LOL

---
+++ Assets/Scripts/Services/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    private  IEnumerator LoadScene()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("TestLevel", LoadSceneMode.Single);
+        asyncOperation.allowSceneActivation = false;
+        
+        while (!asyncOperation.isDone)
+        {
+            if (asyncOperation.progress >= 0.9f)
+            {
+                DOTween.Sequence().PrependInterval(0.3f).OnComplete(() =>
+                {
+                    asyncOperation.allowSceneActivation = true;
+                });
+            }
+            
+            yield return null;
+        }
+    }
+}
